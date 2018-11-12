8916f39cb26bdc2b87d17ee08e6b032c8888acea
dantogno <david.antognoli@gmail.com>
2018-11-11 18:40:30 -0600

    added svg text background and line renderer

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    [SerializeField]
+    private Transform endpoint0;
+    [SerializeField]
+    private Transform endpoint1;
+    private LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+ 
+	}
+
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, endpoint0.position);
+        lineRenderer.SetPosition(1, endpoint1.position);
+    }
+
+}

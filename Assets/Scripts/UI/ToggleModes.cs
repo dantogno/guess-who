72dc1330d86fe9f41e9a5c630f392cbeedeb0d13
David Antognoli <david.antognoli@gmail.com>
2018-11-20 10:25:38 -0600

    Fixed a bug with toggling modes from various animation states. Hide toggle modes when charge menu is open.

--- Assets/Scripts/UI/ToggleModes.cs
+++ Assets/Scripts/UI/ToggleModes.cs
@@ -5,11 +5,42 @@ using UnityEngine;
 public class ToggleModes : MonoBehaviour
 {
     [SerializeField]
-    private GameObject suspectInterfacePanel, arCamera;
+    private GameObject arCamera;
+
+    [SerializeField]
+    private CanvasGroup suspectInterfaceCanvasGroup, toggleModesCanvasGroup;
+
+    private void OnEnteredChargeSuspectState()
+    {
+        SetToggleModesCanvasGroupVisibility(false);
+    }
+    private void OnExitedChargeSuspectState()
+    {
+        SetToggleModesCanvasGroupVisibility(true);
+    }
+    private void OnEnable()
+    {
+        ChargeSuspectPopupBehaviour.EnteredChargeSuspectState += OnEnteredChargeSuspectState;
+        ChargeSuspectPopupBehaviour.ExitedChargeSuspectState += OnExitedChargeSuspectState;
+    }
+    private void OnDisable()
+    {
+        ChargeSuspectPopupBehaviour.EnteredChargeSuspectState -= OnEnteredChargeSuspectState;
+        ChargeSuspectPopupBehaviour.ExitedChargeSuspectState -= OnExitedChargeSuspectState;
+    }
+
+    private void SetToggleModesCanvasGroupVisibility(bool isVisible)
+    {
+        toggleModesCanvasGroup.alpha = isVisible ? 1 : 0;
+        toggleModesCanvasGroup.blocksRaycasts = isVisible;
+        toggleModesCanvasGroup.interactable = isVisible;
+    }
 
     public void SuspectListToggled(bool isOn)
     {
-        suspectInterfacePanel.SetActive(isOn);
+        suspectInterfaceCanvasGroup.alpha = isOn ? 1 : 0;
+        suspectInterfaceCanvasGroup.blocksRaycasts = isOn;
+        suspectInterfaceCanvasGroup.interactable = isOn;
         arCamera.SetActive(!isOn);
     }
 }

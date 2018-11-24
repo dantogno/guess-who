278f9a2fb90228b3bf169e9c1e454ac4facbf467
dantogno <david.antognoli@gmail.com>
2018-11-24 10:16:31 -0500

    quick and dirty "scanning indicator" and we are sucesssfully scanning at this point.

--- Assets/Scripts/CrimeSceneTrackableEventHandler.cs
+++ Assets/Scripts/CrimeSceneTrackableEventHandler.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class CrimeSceneTrackableEventHandler : DefaultTrackableEventHandler
 {
+    [SerializeField]
+    private GameObject trackingIndicator;
+
     protected override void OnTrackingFound()
     {
+        trackingIndicator.SetActive(true);
         var rendererComponents = GetComponentsInChildren<Renderer>(true);
         var colliderComponents = GetComponentsInChildren<Collider>(true);
         var canvasComponents = GetComponentsInChildren<Canvas>(true);
@@ -24,6 +28,12 @@ public class CrimeSceneTrackableEventHandler : DefaultTrackableEventHandler
         // Enable canvas':
         foreach (var component in canvasComponents)
             component.enabled = true;
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        trackingIndicator.SetActive(false);
     }
 
 }
